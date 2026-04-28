101b881e8c13773b2bbeb0aa4a7af8951ebae127
Fernanda Rizzetto <272921191+fernandarizzetto@users.noreply.github.com>
2026-04-28 21:49:58 +0100

    update codigo e removendo arquivo requirement.txt

--- app.py
+++ app.py
@@ -2,4 +2,18 @@ import pandas as pd
 import streamlit as st
 import plotly.express as px
 
-car_data = pd.read_csv('vehicles.csv')  # Lendo os dados
+car_data = pd.read_csv('notebooks/vehicles.csv')  # Lendo os dados
+
+st.header('Análise de Dados de Veículos')  # Título do aplicativo
+
+hist_button = st.button('Criar histograma')  # Criar um botão
+
+if hist_button:  # se o botão for clicado
+    # escrever uma mensagem
+    st.write('Criando um histograma para o conjunto de Análise de Dados de Veículos')
+
+    # criar um histograma
+    fig = px.histogram(car_data, x="odometer")
+
+    # exibir um gráfico Plotly interativo
+    st.plotly_chart(fig, use_container_width=True)
